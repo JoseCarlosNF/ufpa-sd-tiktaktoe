b778d90f7ba1870a40691e128f2250b6acdb762a
Jose Noronha <jnoronha@uolinc.com>
2023-12-05 09:06:04 -0300

    feat(init): define interface de interação
    Criação e ingresso em jogo

---
+++ ufpa_sd_tiktaktoe/__main__.py
@@ -0,0 +1,29 @@
+import argparse
+
+from tiktaktoe import Game
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='tik-tak-toe',
+        description='A online version of a classic tic-tac-toe game.',
+    )
+    parser.add_argument(
+        '-c',
+        '--create',
+        action='store_true',
+        help='Create a new game.',
+    )
+    parser.add_argument(
+        '-j',
+        '--join',
+        help='Join a game using UUID.',
+    )
+    args = parser.parse_args()
+    game = Game()
+
+    if args.create:
+        game.new()
+        game.loop('new_game')
+    else:
+        game.join(args.join)
+        game.loop('join_game')
