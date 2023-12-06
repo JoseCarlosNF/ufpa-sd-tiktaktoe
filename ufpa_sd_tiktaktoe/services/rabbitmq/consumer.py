b99ba01ff88aedc5bc72077df3ca08a0e135a7e9
Jose Noronha <jnoronha@uolinc.com>
2023-12-06 07:30:22 -0300

    feat(rabbitmq): declara exchange e TTL para as filas

--- ufpa_sd_tiktaktoe/services/rabbitmq/consumer.py
+++ ufpa_sd_tiktaktoe/services/rabbitmq/consumer.py
@@ -2,16 +2,18 @@ import os
 
 import pika
 
+QUEUE_TTL = 15 * 60 * 1000
+
 
 class RabbitmqConsumer:
-    def __init__(self, callback) -> None:
+    def __init__(self, queue, callback) -> None:
         self.__host = os.environ.get('RABBITMQ_HOST', 'localhost')
         self.__port = os.environ.get('RABBITMQ_PORT', 5672)
         self.__usernamer = os.environ.get('RABBITMQ_USERNAME', 'bugs')
         self.__password = os.environ.get('RABBITMQ_PASSWORD', 'bunny')
-        self.__queue = ''
+        self.__queue = queue
         self.__callback = callback
-        self.__channel = self.__create_channel()
+        self.channel = self.__create_channel()
 
     def __create_channel(self):
         connection_parameters = pika.ConnectionParameters(
@@ -22,7 +24,12 @@ class RabbitmqConsumer:
             ),
         )
         channel = pika.BlockingConnection(connection_parameters).channel()
-        channel.queue_declare(queue=self.__queue, durable=True)
+        channel.queue_declare(
+            queue=self.__queue,
+            durable=False,
+            arguments={'x-expires': QUEUE_TTL},
+        )
+        channel.stop_consuming()
         channel.basic_consume(
             queue=self.__queue,
             auto_ack=True,
@@ -31,4 +38,4 @@ class RabbitmqConsumer:
         return channel
 
     def start(self):
-        self.__channel.start_consuming()
+        self.channel.start_consuming()
--- ufpa_sd_tiktaktoe/services/rabbitmq/publisher.py
+++ ufpa_sd_tiktaktoe/services/rabbitmq/publisher.py
@@ -3,15 +3,17 @@ import os
 
 import pika
 
+QUEUE_TTL = 15 * 60 * 1000
+
 
 class RabbitmqPublisher:
-    def __init__(self) -> None:
+    def __init__(self, routing_key: str) -> None:
         self.__host = os.environ.get('RABBITMQ_HOST', 'localhost')
         self.__port = os.environ.get('RABBITMQ_PORT', 5672)
         self.__usernamer = os.environ.get('RABBITMQ_USERNAME', 'bugs')
         self.__password = os.environ.get('RABBITMQ_PASSWORD', 'bunny')
         self.__exchange = 'tiktaktoe'
-        self.__routing_key = ''
+        self.__routing_key = routing_key
         self.__channel = self.__create_channel()
 
     def __create_channel(self):
@@ -23,6 +25,18 @@ class RabbitmqPublisher:
             ),
         )
         channel = pika.BlockingConnection(connection_parameters).channel()
+        channel.exchange_declare(
+            exchange=self.__exchange, exchange_type='direct', durable=False
+        )
+        channel.queue_declare(
+            self.__routing_key, arguments={'x-expires': QUEUE_TTL}
+        )
+        channel.queue_bind(
+            self.__routing_key,
+            self.__exchange,
+            self.__routing_key,
+            arguments={'x-expires': QUEUE_TTL},
+        )
         return channel
 
     def send_message(self, body: dict):
